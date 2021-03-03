287cd0a3e0f03298d164877e45f963d2a0c6abe8
Abe Rycus <arycus@Abes-MacBook-Air.local>
2021-03-03 11:42:55 -0500

    react testttt

---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyDrdbVKFZxmLDe8JAlgzyp8AXnTBWDYnkA",
+    authDomain: "react-test-72835.firebaseapp.com",
+    databaseURL: "https://react-test-72835-default-rtdb.firebaseio.com"
+  };
+  
+  export default config;
+  
